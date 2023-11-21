c772887274eaf28307629d8adddc4747bcfa75c2
test-dan-run <daniel.leongsj@gmail.com>
2023-11-22 00:10:14 +0800

    refactor: shift and rename gradio api inference test

---
+++ tests/gr_single_inference.py
@@ -0,0 +1,19 @@
+import base64
+import requests
+
+FILENAME = "../examples/busy_street_cook_16k.wav"
+
+with open(FILENAME, mode="rb") as f:
+    audio_encoded = base64.b64encode(f.read())
+
+encoded_string = f"data:audio/wav;base64,{str(audio_encoded, 'utf-8')}"
+
+r = requests.post(
+    url='http://localhost:8080/api/predict/', 
+    json={
+        "data": [
+            {"data": encoded_string, "name": FILENAME}]
+    }, timeout = 30)
+r.json()
+
+print(r.json())
